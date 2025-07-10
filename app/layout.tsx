8bc44776a450f76cfe6e19786f9adaa590816baf
Mubashirmreal <mubashirahmed1598@gmail.com>
2025-07-10 19:17:54 +0530

    Edited layout.tsx to add title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Scrapkart',
+  description: 'Scrapkart is a platform for buying and selling scrap materials.',
+  generator: 'Mubashir',
 }
 
 export default function RootLayout({
